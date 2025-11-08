d818611307485122462e62edb5931d294dd1114a
retore404 <44674058+retore404@users.noreply.github.com>
2025-11-08 03:28:23 +0000

    add: Predict

---
+++ 01_04_predict.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import dspy
+import os
+
+load_dotenv()
+
+def main():
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    lm = dspy.LM("openai/gpt-4o-mini", api_key=OPENAI_API_KEY, cache=False)
+    dspy.configure(lm=lm)
+    math = dspy.Predict("question -> answer: float")
+    print(math(question="2つのサイコロを振った時，2つの出目の合計が2になる確率は？"))
+    dspy.inspect_history(n=1)
+
+if __name__ == "__main__":
+    main()
