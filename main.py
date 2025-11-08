3d9fffdc1dea3ce76cc8aca355d9ae2f073b5c65
retore404 <44674058+retore404@users.noreply.github.com>
2025-11-08 01:46:30 +0000

    シンプルな応答

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from dotenv import load_dotenv
+import dspy
+import os
+
+load_dotenv()
+
 def main():
     print("Hello from dspy-training!")
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    lm = dspy.LM("openai/gpt-4o-mini", api_key=OPENAI_API_KEY)
+    dspy.configure(lm=lm)
+    print(lm("Say this is a test!", temperature=0.7))  # => ['This is a test!']
+    print(lm(messages=[{"role": "user", "content": "Say this is a test!"}]))  # => ['This is a test!']
 
 
 if __name__ == "__main__":
