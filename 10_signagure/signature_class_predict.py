b917be0314ef20b8f91cbb476f3cd157f9500cb1
retore404 <44674058+retore404@users.noreply.github.com>
2025-11-15 04:38:38 +0000

    signature

---
+++ 10_signagure/signature_class_predict.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import dspy
+import os
+from langfuse import get_client
+from openinference.instrumentation.dspy import DSPyInstrumentor
+
+load_dotenv()
+
+# Observe with Langfuse
+langfuse = get_client()
+DSPyInstrumentor().instrument()
+
+class Answer(dspy.Signature):
+    """Answer the question."""
+    question: str = dspy.InputField()
+    answer: float = dspy.OutputField()
+
+def main():
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    lm = dspy.LM("openai/gpt-4o-mini", api_key=OPENAI_API_KEY, cache=False)
+    dspy.configure(lm=lm)
+    math = dspy.Predict(Answer)
+    math.save("./test.json")
+    print(math(question="2つのサイコロを振った時，2つの出目の合計が4になる確率は？"))
+    dspy.inspect_history(n=1)
+
+if __name__ == "__main__":
+    main()
