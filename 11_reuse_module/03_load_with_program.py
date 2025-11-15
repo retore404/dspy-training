66cb25fa34504375ae65f8e239d30d826e626016
retore404 <44674058+retore404@users.noreply.github.com>
2025-11-15 03:00:40 +0000

    signature and save module

---
+++ 11_reuse_module/03_load_with_program.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import dspy
+import os
+from langfuse import get_client
+from openinference.instrumentation.dspy import DSPyInstrumentor
+
+load_dotenv()
+
+# Observe with Langfuse
+langfuse = get_client()
+DSPyInstrumentor().instrument()
+
+def main():
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    lm = dspy.LM("openai/gpt-4o-mini", api_key=OPENAI_API_KEY, cache=False)
+    dspy.configure(lm=lm)
+    math = dspy.load("./artifacts/with_program/")
+    print(math(question="2つのサイコロを振った時，2つの出目の合計が4になる確率は？"))
+    dspy.inspect_history(n=1)
+
+if __name__ == "__main__":
+    main()
